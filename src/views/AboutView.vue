5ed6af67dc7330cea7fbb378da57ccaeae45916f
maya <maya.krey@gmail.com>
2023-05-18 11:46:10 -0600

    Remove unused code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
     <h1 class="title">about me</h1>
-  </div>
+    <div class="text-wrapper">
+      <p>My name is Maya</p>
+      <p>If you are looking for a passionate team player, send me a message via e-mail or on LinkedIn.</p>
+      <p>I’ll be happy to have a chat.</p>
+    </div>
 </template>
