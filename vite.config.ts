c79b3446728ffc2c0be8980cc12df27afb4c06f7
maya <maya.krey@gmail.com>
2023-05-24 15:33:33 -0600

    Add publicDir

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: false,
+  publicDir: '<root>/portfolio/',
   build: {
     outDir: 'dist',
     rollupOptions: {
