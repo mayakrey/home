91a8b0759bedb180941f9b400976a7937db53205
maya <maya.krey@gmail.com>
2023-05-24 15:19:39 -0600

    Play around with config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: false,
   build: {
     rollupOptions: {
       input: {
